26a9d330322116ff91cb483437a2657a50c04c5f
mitchellkevin <mitchell.scholte04@gmail.com>
2026-05-08 13:07:55 +0200

    ok

--- src/scripts/.!98333!multiplayer.js
+++
@@ -1,7 +0,0 @@
-const token = localStorage.getItem('token');
-if (!token) { location.href = '/login'; }
-
-const params = new URLSearchParams(location.search);
-const roomId = params.get('room');
-if (!roomId || !/^\d+$/.test(roomId)) { location.href = '/'; }
-
